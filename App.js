6dc63bc96a22c60938e675d209d02cc7ef2e3759
Pavel Belousov <signorpao@gmail.com>
2023-02-23 20:13:14 +0300

    settings...

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
+  const handleButton = () => console.log("Button clicked");
+
+  const handleButtonPress = () =>
+    Alert.alert("PushMessage", "Are you ready?", [
+      { text: "Yeah" },
+      { text: "No" },
+    ]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Hello {"\n"}motherfuckers!!!</Text>
@@ -9,6 +18,12 @@ export default function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. {"\n"}Lorem
         ipsum dolor sit amet consectetur adipisicing elit.
       </Text>
+      <Button title="Press" color="red" onPress={handleButton} />
+      <Button
+        title="Press again dude"
+        color="green"
+        onPress={handleButtonPress}
+      />
 
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -19,11 +34,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
+    alignItems: "center",
     // justifyContent: "center",
   },
   title: {
     color: "blue",
     fontSize: "30px",
   },
+  // button: {
+  //   width: "200px",
+  // },
 });
